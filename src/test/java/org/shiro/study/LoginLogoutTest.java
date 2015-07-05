ae5c4670fc450e484172861fc304a8bfdf7e49ec
fxr <fxr@fxr-PC>
2015-07-05 19:07:59 +0800

    用户名密码数据库获得测试用例

--- src/test/java/org/shiro/study/LoginLogoutTest.java
+++ src/test/java/org/shiro/study/LoginLogoutTest.java
@@ -6,6 +6,8 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.apache.shiro.mgt.SecurityManager;
@@ -37,5 +39,37 @@ public class LoginLogoutTest {
 		
 		// 6、退出
 		subject.logout();
+	}
+	
+	@Test
+	public void testJDBCRealm() {
+		// 1、获取SecurityManager工厂，此处使用ini配置文件初始化SecurityManager
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-jdbc-realm.ini");
+		
+		// 2、得到SecurityManager实例，并绑定给SecurityUtils
+		SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		
+		// 3、得到Subject及创建用户名/密码身份验证Token(即用户身份/凭证)
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+		
+		try {
+			// 4、登录，即身份验证
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			// 5、身份验证失败
+		}
+		
+		Assert.assertEquals(true, subject.isAuthenticated()); // 断言用户已经登录
+		
+		// 6、退出
+		subject.logout();
+	}
+	
+	@After
+	public void tearDown() {
+		// 退出时请解除绑定Subject到线程，否则对下次测试造成影响
+		ThreadContext.unbindSubject();
 	}
 }
